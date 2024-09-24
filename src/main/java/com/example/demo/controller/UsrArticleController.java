e43219b1eeaa95d0e2aafbadbab1b7e2653d625a
sooooo56 <sooooo4645@gmail.com>
2024-09-24 19:13:46 +0900

    doWrite, writeArticle, makeTestData, doDelete, deleteArticle, doModify, modifyArticle

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -9,33 +9,93 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.vo.Article;
 
-
 @Controller
 public class UsrArticleController {
+	
 	private int lastArticleId;
 	private List<Article> articles;
 	
 	public UsrArticleController() {
 		this.lastArticleId = 0;
 		this.articles = new ArrayList<>();
+		
+		makeTestData();
 	}
 	
-	@GetMapping("/usr/article/doWrite")
-	@ResponseBody
-	public Article doWrite(String title, String body) {
+	private void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			String title = "제목" + i;
+			String body = "내용" + i;
+			
+			writeArticle(title, body);
+		}
+	}
+	
+	private Article writeArticle(String title, String body) {
+		
 		Article article = new Article(++lastArticleId, title, body);
 		this.articles.add(article);
 		
 		return article;
 	}
 	
+	private void modifyArticle(Article foundArticle, String title, String body) {
+		foundArticle.setTitle(title);
+		foundArticle.setBody(body);
+	}
+	
+	private void deleteArticle(Article foundArticle) {
+		this.articles.remove(foundArticle);
+	}
+	
+	private Article getArticleById(int id) {
+		for (Article article : this.articles) {
+			if (article.getId() == id) {
+				return article;
+			}
+		}
+		return null;
+	}
+	
+	@GetMapping("/usr/article/doWrite")
+	@ResponseBody
+	public Article doWrite(String title, String body) {
+		return writeArticle(title, body);
+	}
+	
 	@GetMapping("/usr/article/showList")
 	@ResponseBody
-	public List<Article> list() {
-		
+	public List<Article> showList() {
 		return this.articles;
 	}
 	
-}
+	@GetMapping("/usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+		
+		Article foundArticle = getArticleById(id);
+		
+		if (foundArticle == null) {
+			return id + "번 게시물은 존재하지 않습니다";
+		}
+		
+		modifyArticle(foundArticle, title, body);
+		
+		return id + "번 게시물을 수정했습니다";
+	}
 
-//http://localhost:8080/usr/article/showList
+	@GetMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		
+		Article foundArticle = getArticleById(id);
+		
+		if (foundArticle == null) {
+			return id + "번 게시물은 존재하지 않습니다";
+		}
+		
+		deleteArticle(foundArticle);
+		
+		return id + "번 게시물을 삭제했습니다";
+	}
+}
